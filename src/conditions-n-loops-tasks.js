1186cc428415f32e86159bd91b8caf6dbf794d62
giorgi khoshtaria <99177635+g1org11@users.noreply.github.com>
2024-12-25 12:29:04 +0400

    fear: 5 passing

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -21,8 +21,8 @@
  *  0  => true
  *  -5 => false
  */
-function isPositive(/* number */) {
-  throw new Error('Not implemented');
+function isPositive(number) {
+  return number >= 0;
 }
 
 /**
@@ -38,8 +38,14 @@ function isPositive(/* number */) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  if (a >= b && a >= c) {
+    return a;
+  }
+  if (b >= a && b >= c) {
+    return b;
+  }
+  return c;
 }
 
 /**
@@ -60,8 +66,12 @@ function getMaxNumber(/* a, b, c */) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  return (
+    queen.x === king.x ||
+    queen.y === king.y ||
+    Math.abs(queen.x - king.x) === Math.abs(queen.y - king.y)
+  );
 }
 
 /**
@@ -82,8 +92,8 @@ function canQueenCaptureKing(/* queen, king */) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  return a + b + c > Math.max(a, b, c) * 2;
 }
 
 /**
@@ -100,8 +110,42 @@ function isIsoscelesTriangle(/* a, b, c */) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  let romanNum = '';
+  const romanDec = Math.floor(num / 10);
+  const romanUnit = num % 10;
+  let decCount = '';
+  let unitsCount = '';
+
+  if (num >= 10) {
+    for (let i = 1; i <= romanDec; i += 1) {
+      decCount += 'X';
+    }
+  }
+
+  if (romanUnit) {
+    if (romanUnit <= 3) {
+      for (let i = 1; i <= romanUnit; i += 1) {
+        unitsCount += 'I';
+      }
+    } else if (romanUnit === 4) {
+      unitsCount = 'IV';
+    } else if (romanUnit === 5) {
+      unitsCount = 'V';
+    } else if (romanUnit >= 6 && romanUnit <= 8) {
+      unitsCount = 'V';
+
+      for (let i = 6; i <= romanUnit; i += 1) {
+        unitsCount += 'I';
+      }
+    } else if (romanUnit === 9) {
+      unitsCount = 'IX';
+    }
+  }
+
+  romanNum = decCount + unitsCount;
+
+  return romanNum;
 }
 
 /**
